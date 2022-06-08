b15d2bb7e204baf54e3c051498758d9250a254d3
Kirill Krylov <k.krylov@creatio.com>
2022-06-08 17:14:24 +0300

    init

---
+++ DemoPipeline.Tests/Class1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Terrasoft.Configuration.Tests;
+
+namespace DemoPipeline.Tests
+{
+	[TestFixture]
+	[MockSettings(RequireMock.All)]
+	public class DemoServiceTests : BaseConfigurationTestFixture
+	{
+
+		private DemoService _sut;
+		protected override void SetUp()
+		{
+			base.SetUp();
+			_sut = new DemoService
+			{
+				HttpContextAccessor = SetupHttpContextAccessor(UserConnection)
+			};
+		}
+
+		[Test(Author = "Kirill Krylov", Description = "endpoint returns expecte value")]
+		public void GetMethodExample_ShouldReturn()
+		{
+			//Arrange
+			string expected = "OK";
+
+			//Act
+			var actual = _sut.GetMethodExample();
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+	}
+}
---
+++ DemoPipeline/Files/cs/DemoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using Terrasoft.Core;
+using Terrasoft.Web.Common;
+
+namespace DemoPipeline
+{
+	[ServiceContract]
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+	public class DemoService : BaseService
+	{
+		[OperationContract]
+		[WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+		public string GetMethodExample()
+		{
+			return "OK";
+		}
+	}
+}
